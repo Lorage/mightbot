6a9051ff4f9cf03ebe1d6fd828a5fb2e831b62f6
Caleb Cochran <k1kokhran@gmail.com>
2017-05-18 02:07:27 -0500

    Added command reading from JSON file

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net"
 	"net/textproto"
 	"os"
@@ -22,33 +24,46 @@ func removeIndex(s []string, index int) []string {
 	return append(s[:index], s[index+1:]...)
 }
 
+type commandObject struct {
+	User     string
+	Command  string
+	Response string
+}
+
 func main() {
-	// connect to the twitch server
+	// Connect to the twitch server
 	conn, err := net.Dial("tcp", "irc.chat.twitch.tv:6667")
 	if err != nil {
 		panic(err)
 	}
 
-	// token, username, channel
+	// Token, username, channel
 	conn.Write([]byte("PASS " + "oauth:" + os.Getenv("BOT_OAUTH_TOKEN") + "\r\n"))
 	conn.Write([]byte("NICK " + os.Getenv("BOT_ACCOUNT_NAME") + "\r\n"))
 	conn.Write([]byte("JOIN " + os.Getenv("CHANNEL_NAME") + "\r\n"))
 	defer conn.Close()
 
-	// handles reading from the connection
+	// Handles reading from the connection
 	tp := textproto.NewReader(bufio.NewReader(conn))
 
-	//Check for spam/message frequency
-	//Load JSON file with basic text command and responses
-	/*
-		[
-			{
-				"command": "!thiscommand",
-				"user": "username",
-				"response": "Response text"
-			}
-		]
-	*/
+	//TODO: Check for spam/message frequency
+
+	filePath := "./commands.json"
+	fmt.Printf("// Reading file %s\n", filePath)
+	file, fileReadError := ioutil.ReadFile(filePath)
+	if fileReadError != nil {
+		fmt.Printf("// Error while reading file %s\n", filePath)
+		fmt.Printf("File error: %v\n", fileReadError)
+		os.Exit(1)
+	}
+
+	var commands []commandObject
+
+	readJSONError := json.Unmarshal(file, &commands)
+	if readJSONError != nil {
+		fmt.Println("Error:", readJSONError)
+		os.Exit(1)
+	}
 
 	for {
 		msg, err := tp.ReadLine()
@@ -56,23 +71,30 @@ func main() {
 			panic(err)
 		}
 
-		// split the msg by spaces
+		// Split the msg by spaces
 		msgParts := strings.Split(msg, " ")
 
 		fmt.Println(msgParts)
 
-		// if the msg contains PING you're required to
-		// respond with PONG else you get kicked
+		// Respond with PONG
 		if msgParts[0] == "PING" {
 			conn.Write([]byte("PONG " + msgParts[1]))
 			continue
 		}
 
-		// if msg contains PRIVMSG then respond
+		// If msg contains PRIVMSG then respond
 		if msgParts[1] == "PRIVMSG" {
-			// echo back the same message
+			var newMessage string
 			messageText := strings.Split(msg, os.Getenv("CHANNEL_NAME")+" :")
-			var message = []byte("PRIVMSG " + os.Getenv("CHANNEL_NAME") + " :" + messageText[1] + "\r\n")
+
+			for command := range commands {
+				var newString = strings.Join(messageText, "")
+				if strings.Contains(newString, commands[command].Command) {
+					newMessage = commands[command].Response
+				}
+			}
+
+			var message = []byte("PRIVMSG " + os.Getenv("CHANNEL_NAME") + " :" + newMessage + "\r\n")
 			conn.Write(message)
 		}
 	}
