e95c05bacc21a106d0c40d4dbb2bc75ef4b0a07a
Caleb Cochran <k1kokhran@gmail.com>
2017-06-04 22:23:58 -0500

    Implemented distributed architecture for creating bots

--- main.go
+++ main.go
@@ -5,8 +5,9 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
-	"io/ioutil"
+	"log"
 	"net"
+	"net/http"
 	"net/textproto"
 	"os"
 	"strings"
@@ -25,13 +26,28 @@ func removeIndex(s []string, index int) []string {
 	return append(s[:index], s[index+1:]...)
 }
 
-type commandObject struct {
-	User     string
+// Structure of []Commands
+type CommandObject struct {
 	Command  string
 	Response string
 }
 
-func main() {
+// Structure of bot creator's submission
+type BotInfo struct {
+	Token         string          `json:"token"`
+	BotName       string          `json:"botName"`
+	TargetChannel string          `json:"targetChannel"`
+	Identifier    string          `json:"identifier"`
+	Commands      []CommandObject `json:"commands"`
+}
+
+// Response details
+type ServerResponse struct {
+	Message string
+	Details string
+}
+
+func startBot(token string, botName string, targetChannel string, commands []CommandObject) {
 	// Connect to the twitch server
 	conn, err := net.Dial("tcp", "irc.chat.twitch.tv:6667")
 	if err != nil {
@@ -39,32 +55,15 @@ func main() {
 	}
 
 	// Token, username, channel
-	conn.Write([]byte("PASS " + "oauth:" + os.Getenv("BOT_OAUTH_TOKEN") + "\r\n"))
-	conn.Write([]byte("NICK " + os.Getenv("BOT_ACCOUNT_NAME") + "\r\n"))
-	conn.Write([]byte("JOIN " + os.Getenv("CHANNEL_NAME") + "\r\n"))
+	conn.Write([]byte("PASS " + "oauth:" + token + "\r\n"))
+	conn.Write([]byte("NICK " + botName + "\r\n"))
+	conn.Write([]byte("JOIN " + targetChannel + "\r\n"))
 	defer conn.Close()
 
 	// Handles reading from the connection
 	tp := textproto.NewReader(bufio.NewReader(conn))
 
 	//TODO: Check for spam/message frequency
-
-	filePath := "./commands.json"
-	fmt.Printf("// Reading file %s\n", filePath)
-	file, fileReadError := ioutil.ReadFile(filePath)
-	if fileReadError != nil {
-		fmt.Printf("// Error while reading file %s\n", filePath)
-		fmt.Printf("File error: %v\n", fileReadError)
-		os.Exit(1)
-	}
-
-	var commands []commandObject
-
-	readJSONError := json.Unmarshal(file, &commands)
-	if readJSONError != nil {
-		fmt.Println("Error:", readJSONError)
-		os.Exit(1)
-	}
 
 	for {
 		msg, err := tp.ReadLine()
@@ -74,18 +73,17 @@ func main() {
 			panic(err)
 		}
 
-		// Split the msg by spaces
 		msgParts := strings.Split(msg, " ")
 
+		// For logging purposes
 		fmt.Println(msgParts)
 
-		// Respond with PONG
+		// Respond with PONG required
 		if msgParts[0] == "PING" {
 			conn.Write([]byte("PONG " + msgParts[1]))
 			continue
 		}
 
-		// If msg contains PRIVMSG then respond
 		if msgParts[1] == "PRIVMSG" {
 			var newMessage string
 			messageText := strings.Split(msg, os.Getenv("CHANNEL_NAME")+" :")
@@ -101,4 +99,47 @@ func main() {
 			conn.Write(message)
 		}
 	}
+}
+
+func main() {
+	h := http.NewServeMux()
+
+	h.HandleFunc("/register", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "A-MEI-zing!")
+	})
+
+	h.HandleFunc("/login", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Raising my APM!")
+	})
+
+	h.HandleFunc("/createBot", func(w http.ResponseWriter, r *http.Request) {
+		/* TODO: Check for ->
+		// command format (not empty) (return error stating that the commands are empty)
+		// check for existing bots for identifier/signature (limit one per user)
+		// limit time for bot run time (2-6 hours)
+		// allow a user to kill a bot they created
+		*/
+		var botInfo BotInfo
+		if r.Body == nil {
+			http.Error(w, "Please send a request body", 400)
+			return
+		}
+		err := json.NewDecoder(r.Body).Decode(&botInfo)
+		if err != nil {
+			http.Error(w, err.Error(), 400)
+			return
+		}
+
+		go startBot(botInfo.Token, botInfo.BotName, botInfo.TargetChannel, botInfo.Commands)
+	})
+
+	h.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(404)
+		res := ServerResponse{Message: "Route error", Details: "No endpoint at this location"}
+		json.NewEncoder(w).Encode(res)
+	})
+
+	err := http.ListenAndServe(":7000", h)
+	log.Println("Listening...")
+	log.Fatal(err)
 }
