9dce84b6e47a5075b0fe7bd7a013486326171799
Caleb Cochran <k1kokhran@gmail.com>
2017-07-06 21:25:27 -0500

    Split bot logic into a package and functionized

---
+++ botlogic/botlogic.go
@@ -0,0 +1,111 @@
+package botlogic
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"net/textproto"
+	"os"
+	"strings"
+	"time"
+)
+
+type BotRecord struct {
+	BotChannel chan string
+	UUID       string
+}
+
+// Structure of []Commands
+type CommandObject struct {
+	Command  string
+	Response string
+}
+
+func checkForCommand(message string) string {
+	var commandMessage []string
+	if strings.HasPrefix(message, "!") {
+		commandMessage = strings.SplitAfter(message, "!")
+	}
+
+	return strings.Join(commandMessage, "")
+}
+
+func StartBot(token string, botName string, targetChannel string, commands []CommandObject, botRecord BotRecord) {
+	var routineTimer int
+	// Connect to the twitch server
+	conn, err := net.Dial("tcp", "irc.chat.twitch.tv:6667")
+	if err != nil {
+		panic(err)
+	}
+
+	// Token, username, channel
+	conn.Write([]byte("PASS " + "oauth:" + token + "\r\n"))
+	conn.Write([]byte("NICK " + botName + "\r\n"))
+	conn.Write([]byte("JOIN " + targetChannel + "\r\n"))
+	defer conn.Close()
+
+	// Handles reading from the connection
+	tp := textproto.NewReader(bufio.NewReader(conn))
+
+	// Rewrite to remove ticker, use time diff instead
+	ticker := time.NewTicker(time.Minute)
+	go func() {
+		for t := range ticker.C {
+			routineTimer++
+			log.Println(t)
+		}
+	}()
+
+	for {
+		select {
+		case channelResult := <-botRecord.BotChannel:
+			switch {
+			case channelResult == "refresh":
+				routineTimer = 0
+			case channelResult == "close":
+				return
+			}
+		default:
+			break
+		}
+
+		if routineTimer >= 30 {
+			return
+		}
+
+		msg, err := tp.ReadLine()
+		if err == io.EOF {
+			continue
+		} else if err != nil {
+			panic(err)
+		}
+
+		msgParts := strings.Split(msg, " ")
+
+		// For logging purposes
+		fmt.Println(msgParts)
+
+		// Respond with PONG required
+		if msgParts[0] == "PING" {
+			conn.Write([]byte("PONG :tmi.twitch.tv\r\n"))
+			continue
+		}
+
+		if msgParts[1] == "PRIVMSG" {
+			var newMessage string
+			messageText := strings.Split(msg, os.Getenv("CHANNEL_NAME")+" :")
+
+			for command := range commands {
+				var newString = strings.Join(messageText, "")
+				if strings.Contains(newString, commands[command].Command) {
+					newMessage = commands[command].Response
+				}
+			}
+
+			var message = []byte("PRIVMSG " + os.Getenv("CHANNEL_NAME") + " :" + newMessage + "\r\n")
+			conn.Write(message)
+		}
+	}
+}
--- main.go
+++ main.go
@@ -1,42 +1,24 @@
 package main
 
 import (
-	"bufio"
 	"encoding/json"
-	"fmt"
-	"io"
 	"log"
-	"net"
+	"mightbot/botlogic"
 	"net/http"
-	"net/textproto"
-	"os"
-	"strings"
-	"time"
 )
-
-type BotRecord struct {
-	BotChannel chan string
-	UUID       string
-}
 
 type ChannelMessage struct {
 	Message string `json:"message"`
 	UUID    string `json:"uuid"`
 }
 
-// Structure of []Commands
-type CommandObject struct {
-	Command  string
-	Response string
-}
-
 // Structure of bot creator's submission
 type BotInfo struct {
-	Token         string          `json:"token"`
-	BotName       string          `json:"botName"`
-	TargetChannel string          `json:"targetChannel"`
-	UUID          string          `json:"uuid"`
-	Commands      []CommandObject `json:"commands"`
+	Token         string                   `json:"token"`
+	BotName       string                   `json:"botName"`
+	TargetChannel string                   `json:"targetChannel"`
+	UUID          string                   `json:"uuid"`
+	Commands      []botlogic.CommandObject `json:"commands"`
 }
 
 // Response details
@@ -45,110 +27,26 @@ type ServerResponse struct {
 	Details string
 }
 
-func checkForCommand(message string) string {
-	var commandMessage []string
-	if strings.HasPrefix(message, "!") {
-		commandMessage = strings.SplitAfter(message, "!")
+func decodeValidate(w http.ResponseWriter, r *http.Request, msgObj interface{}) {
+	if r.Body == nil {
+		http.Error(w, "Please send a request body", 400)
+		return
 	}
 
-	return strings.Join(commandMessage, "")
-}
-
-func startBot(token string, botName string, targetChannel string, commands []CommandObject, botRecord BotRecord) {
-	var routineTimer int
-	// Connect to the twitch server
-	conn, err := net.Dial("tcp", "irc.chat.twitch.tv:6667")
+	err := json.NewDecoder(r.Body).Decode(&msgObj)
 	if err != nil {
-		panic(err)
-	}
-
-	// Token, username, channel
-	conn.Write([]byte("PASS " + "oauth:" + token + "\r\n"))
-	conn.Write([]byte("NICK " + botName + "\r\n"))
-	conn.Write([]byte("JOIN " + targetChannel + "\r\n"))
-	defer conn.Close()
-
-	// Handles reading from the connection
-	tp := textproto.NewReader(bufio.NewReader(conn))
-
-	ticker := time.NewTicker(time.Minute)
-	go func() {
-		for t := range ticker.C {
-			routineTimer++
-			log.Println(t)
-		}
-	}()
-
-	for {
-		select {
-		case channelResult := <-botRecord.BotChannel:
-			switch {
-			case channelResult == "refresh":
-				routineTimer = 0
-			case channelResult == "close":
-				return
-			default:
-				break
-			}
-		default:
-			break
-		}
-
-		if routineTimer >= 30 {
-			return
-		}
-
-		msg, err := tp.ReadLine()
-		if err == io.EOF {
-			continue
-		} else if err != nil {
-			panic(err)
-		}
-
-		msgParts := strings.Split(msg, " ")
-
-		// For logging purposes
-		fmt.Println(msgParts)
-
-		// Respond with PONG required
-		if msgParts[0] == "PING" {
-			conn.Write([]byte("PONG :tmi.twitch.tv\r\n"))
-			continue
-		}
-
-		if msgParts[1] == "PRIVMSG" {
-			var newMessage string
-			messageText := strings.Split(msg, os.Getenv("CHANNEL_NAME")+" :")
-
-			for command := range commands {
-				var newString = strings.Join(messageText, "")
-				if strings.Contains(newString, commands[command].Command) {
-					newMessage = commands[command].Response
-				}
-			}
-
-			var message = []byte("PRIVMSG " + os.Getenv("CHANNEL_NAME") + " :" + newMessage + "\r\n")
-			conn.Write(message)
-		}
+		http.Error(w, err.Error(), 400)
+		return
 	}
 }
 
 func main() {
-	botDirectory := []BotRecord{}
+	botDirectory := []botlogic.BotRecord{}
 	serve := http.NewServeMux()
 
 	serve.HandleFunc("/refresh", func(w http.ResponseWriter, r *http.Request) {
 		var refreshInfo ChannelMessage
-		if r.Body == nil {
-			http.Error(w, "Please send a request body", 400)
-			return
-		}
-
-		err := json.NewDecoder(r.Body).Decode(&refreshInfo)
-		if err != nil {
-			http.Error(w, err.Error(), 400)
-			return
-		}
+		decodeValidate(w, r, &refreshInfo)
 
 		for _, v := range botDirectory {
 			if v.UUID == refreshInfo.UUID {
@@ -159,16 +57,7 @@ func main() {
 
 	serve.HandleFunc("/close", func(w http.ResponseWriter, r *http.Request) {
 		var closeInfo ChannelMessage
-		if r.Body == nil {
-			http.Error(w, "Please send a request body", 400)
-			return
-		}
-
-		err := json.NewDecoder(r.Body).Decode(&closeInfo)
-		if err != nil {
-			http.Error(w, err.Error(), 400)
-			return
-		}
+		decodeValidate(w, r, &closeInfo)
 
 		for _, v := range botDirectory {
 			if v.UUID == closeInfo.UUID {
@@ -178,24 +67,9 @@ func main() {
 	})
 
 	serve.HandleFunc("/createBot", func(w http.ResponseWriter, r *http.Request) {
-		/* TODO: Check for ->
-		// command format (not empty) (return error stating that the commands are empty)
-		// check for existing bots for UUID/signature (limit one per user)
-		// limit for bot run time 30 minutes, refresh pings reset timer to 0
-		// allow a user to kill a bot they created
-		*/
-		var botInfo BotInfo
 		var botExists bool
-		if r.Body == nil {
-			http.Error(w, "Please send a request body", 400)
-			return
-		}
-
-		err := json.NewDecoder(r.Body).Decode(&botInfo)
-		if err != nil {
-			http.Error(w, err.Error(), 400)
-			return
-		}
+		var botInfo BotInfo
+		decodeValidate(w, r, &botInfo)
 
 		for _, val := range botDirectory {
 			if val.UUID == botInfo.UUID {
@@ -208,9 +82,9 @@ func main() {
 		}
 
 		newChannel := make(chan string)
-		newRecord := BotRecord{UUID: botInfo.UUID, BotChannel: newChannel}
+		newRecord := botlogic.BotRecord{UUID: botInfo.UUID, BotChannel: newChannel}
 		botDirectory = append(botDirectory, newRecord)
-		go startBot(botInfo.Token, botInfo.BotName, botInfo.TargetChannel, botInfo.Commands, newRecord)
+		go botlogic.StartBot(botInfo.Token, botInfo.BotName, botInfo.TargetChannel, botInfo.Commands, newRecord)
 	})
 
 	serve.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
